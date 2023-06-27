85a05fe4228b93811ba898a97f18d19d4c3ac7f8
Alex Mumo <alexymumo2000@gmail.com>
2023-06-27 22:55:43 +0300

    added location

--- app/src/main/java/com/alexmumo/saleslifetest/ui/fragments/CustomerLocation.kt
+++ app/src/main/java/com/alexmumo/saleslifetest/ui/fragments/CustomerLocation.kt
@@ -1,25 +1,83 @@
 package com.alexmumo.saleslifetest.ui.fragments
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.navigation.fragment.findNavController
 import com.alexmumo.saleslifetest.R
 import com.alexmumo.saleslifetest.databinding.FragmentCustomerLocationBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class CustomerLocation : Fragment() {
+class CustomerLocation : Fragment(), OnMapReadyCallback {
 
     private lateinit var binding: FragmentCustomerLocationBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var map: GoogleMap
+    private lateinit var currentLocation: Location
+    private lateinit var fusedLocation: FusedLocationProviderClient
+    private var permissionCode = 1
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentCustomerLocationBinding.inflate(layoutInflater)
+        binding = FragmentCustomerLocationBinding.inflate(inflater, container, false)
+        fusedLocation = LocationServices.getFusedLocationProviderClient(requireActivity())
+        getCustomerLocation()
         return binding.root
+    }
+
+    private fun getCustomerLocation() {
+        if (ActivityCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION)!=
+            (PackageManager.PERMISSION_GRANTED) &&
+                    ActivityCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+            (PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), permissionCode)
+            return
+        }
+
+        fusedLocation.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                currentLocation = location
+                val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+                mapFragment.getMapAsync(this)
+                //val mapFragment =  supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+                //mapFragment.getMapAsync(this)
+
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        val latLag =LatLng(currentLocation.latitude, currentLocation.longitude)
+        val markerOptions = MarkerOptions().position(latLag).title("Customer Location")
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLag))
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLag, 15f))
+        googleMap.addMarker(markerOptions)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            permissionCode -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCustomerLocation()
+            }
+        }
     }
 }
